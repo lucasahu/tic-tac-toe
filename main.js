e03dcf70ef3ace7180091c7d05ebbefd094733da
Lucas <lucasignahumada@gmail.com>
2023-08-13 21:26:37 -0400

    Rearrange javascript code structure and add player input storage

---
+++ main.js
@@ -0,0 +1,98 @@
+// GET INPUT FROM USER
+const handleInput = (function(){
+
+    const background = document.querySelector('.background');
+    const card = document.querySelector('.card');
+    const playBtn = document.querySelector('.play-btn');
+    const userNameInput = document.querySelector('#username');
+    const enemyNameInput = document.querySelector('#enemyname');
+    const selectionRadioX = document.querySelector('.selection-x');
+    const startGameBtn = document.querySelector('.start-game-btn');
+
+    function showInput() {
+        background.style.visibility = 'visible'
+        card.style.visibility = 'visible'
+    }
+
+    function hideInput() {
+        background.style.visibility = 'hidden'
+        card.style.visibility = 'hidden'
+    }
+
+    playBtn.addEventListener('click', showInput);
+    startGameBtn.addEventListener('click', hideInput);
+
+    function getInputs() {
+
+        let playerName = userNameInput.value;
+        let enemyName = enemyNameInput.value;
+        let playerSelection;
+        let enemySelection;
+
+        if (selectionRadioX.checked) {
+            playerSelection = 'X';
+            enemySelection = 'O';
+        } else {
+            playerSelection = 'O';
+            enemySelection = 'X';
+        }
+
+        return {playerName, enemyName, playerSelection, enemySelection};
+    }
+
+    function check() {
+        console.log(getInputs().playerName);
+        console.log(getInputs().enemyName);
+        console.log(getInputs().playerSelection);
+        console.log(getInputs().enemySelection);
+    }
+
+    startGameBtn.addEventListener('click', check);
+
+    return {getInputs}
+})();
+
+
+
+const game = (function(){
+
+    const startGameBtn = document.querySelector('.start-game-btn');
+
+    startGameBtn.addEventListener('click', handleInput.getInputs);
+
+    function objectifyData() {
+        let player = {
+            name: handleInput.getInputs().playerName,
+            symbol: handleInput.getInputs().playerSelection,
+            score: 0,
+            active: true
+        }
+
+        let enemy = {
+            name: handleInput.getInputs().enemyName,
+            symbol: handleInput.getInputs().enemySelection,
+            score: 0,
+            active: false
+        }
+        return {player, enemy}
+    }
+
+    startGameBtn.addEventListener('click', objectifyData);
+
+
+    let gameState = ["", "", "", "", "", "", "", "", ""];
+
+//TAKE INPUT AND START GAME
+    //take the input and store it
+    //assign a turn to a player with a boolean (active = false or true)
+
+//EVERY ROUND CHECK FOR WIN CONDITION
+    //when clicking on a grid, render the active players symbol
+    //we check for the win condition
+    //if it is not meet we change active player and keep playing
+
+//UNTIL SOMEONE WINS AND DISPLAY WINNER
+    //when the win condition is meet
+    //display the winner and the game score counter then add 1 to the game score counter
+    //and restart the game
+})();
